5ad9c3ea97bc46f8f6de650b0375244a93255c65
Nikhil Naik <naiknikhil925@gmail.com>
2022-01-13 19:18:55 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-
+import {Switch, Route} from 'react-router-dom';
+ 
 import HomePage from './page/homepage.component';
+import ShopPage from './page/shop/shop.component';
+
 function App() {
   return(
 <div>
-    <HomePage /> 
+<Switch>
+<Route exact path='/' component={HomePage} />
+<Route path='/shop' component={ShopPage} />
+</Switch>
     </div>
-  );
     
-
+  );
 }
 
 export default App;
